7a2730d1a0298872a19cfc162bdf5585fc37f149
Aleksandr Scherba <aleksandr.scherba@iqoption.com>
2021-01-31 16:44:34 +0300

    Add base test class with simple configuration

---
+++ src/test/java/ru/ascherba/epam/events/ui/scenario/BaseTest.java
@@ -0,0 +1,25 @@
+package ru.ascherba.epam.events.ui.scenario;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Created by aleksandr.scherba on 23.01.2021
+ */
+public class BaseTest {
+
+    @BeforeAll
+    static void configureSelenide() {
+        Configuration.baseUrl = "https://events.epam.com";
+    }
+
+    @BeforeEach
+    void setUpSelenideLogger() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .savePageSource(true)
+                .screenshots(true));
+    }
+}
