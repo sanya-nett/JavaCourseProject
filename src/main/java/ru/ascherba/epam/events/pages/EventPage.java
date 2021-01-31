aada934af045a7d703afab08632949f38ff3fb71
Aleksandr Scherba <aleksandr.scherba@iqoption.com>
2021-01-31 16:40:49 +0300

    Make basic pages

---
+++ src/main/java/ru/ascherba/epam/events/pages/EventPage.java
@@ -0,0 +1,70 @@
+package ru.ascherba.epam.events.pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.NotFoundException;
+import ru.ascherba.epam.events.containers.EventCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * Created by aleksandr.scherba on 23.01.2021
+ */
+public class EventPage {
+
+    SelenideElement upcomingEventsTab = $(byText("Upcoming events")).closest(".evnt-tab-item");
+    SelenideElement pastEventsTab = $(byText("Past events")).closest(".evnt-tab-item");
+    ElementsCollection allEventCards = $$(".evnt-cards-container .evnt-card-wrapper");
+    ElementsCollection weekEventCards = $$x("//h3[text()='This Week']/..//div[contains(@class, 'evnt-event-card')]");
+
+    public SelenideElement getUpcomingEventsTab() {
+        return upcomingEventsTab;
+    }
+
+    public SelenideElement getPastEventsTab() {
+        return pastEventsTab;
+    }
+
+    private int getEventTabCounterValue(SelenideElement eventTabElement) {
+        return Integer.parseInt(eventTabElement
+                .$(".evnt-tab-counter")
+                .getText());
+    }
+
+    private List<EventCard> castElementsToEventCards(ElementsCollection eventCardElements) {
+        List<EventCard> eventCardList = new ArrayList<>();
+        for (SelenideElement element : eventCardElements) {
+            eventCardList.add(new EventCard(element));
+        }
+        return eventCardList;
+
+    }
+
+    @Step("Get counter from 'Upcoming events' navigation button")
+    public int getUpcomingEventsCounterValue() {
+        return getEventTabCounterValue(upcomingEventsTab);
+    }
+
+    @Step("Get counter from 'Upcoming events' navigation button")
+    public int getPastEventsCounterValue() {
+        return getEventTabCounterValue(pastEventsTab);
+    }
+
+    @Step("Get all event cards")
+    public List<EventCard> getAllEventCards() {
+        return castElementsToEventCards(allEventCards);
+    }
+
+    @Step("Get 'This week' event cards")
+    public List<EventCard> getWeekEventCards() {
+        if (weekEventCards.isEmpty()) {
+            throw new NotFoundException("Not found week event cards");
+        }
+        return castElementsToEventCards(weekEventCards);
+    }
+}
---
+++ src/main/java/ru/ascherba/epam/events/pages/MainPage.java
@@ -0,0 +1,12 @@
+package ru.ascherba.epam.events.pages;
+
+import ru.ascherba.epam.events.elements.EventPlatformHeader;
+
+/**
+ * Created by aleksandr.scherba on 23.01.2021
+ */
+public class MainPage {
+
+    public EventPlatformHeader platformHeader = new EventPlatformHeader();
+
+}
