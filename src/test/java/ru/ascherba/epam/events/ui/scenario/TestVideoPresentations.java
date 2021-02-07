2613a615214444ffe70a69340b9f14f01802a5cd
Aleksandr Scherba <aleksandr.scherba@iqoption.com>
2021-02-07 22:40:21 +0300

    Update tests from video steps

--- src/test/java/ru/ascherba/epam/events/ui/scenario/TestVideoPresentations.java
+++ src/test/java/ru/ascherba/epam/events/ui/scenario/TestVideoPresentations.java
@@ -1,32 +1,66 @@
 package ru.ascherba.epam.events.ui.scenario;
 
+import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.ascherba.epam.events.entities.EventFilterQueryParams;
+import ru.ascherba.epam.events.entities.talks.EventTalkResponseCard;
+import ru.ascherba.epam.events.helpers.EventCardTalkFilterHelper;
+import ru.ascherba.epam.events.steps.VideoPageSteps;
+
+import java.util.HashMap;
+import java.util.List;
 
 import static io.qameta.allure.Allure.step;
 
 /**
  * Created by aleksandr.scherba on 23.01.2021
  */
+@Epic("EPAM Events")
 @Feature("Страница докладов")
-public class TestVideoPresentations {
+public class TestVideoPresentations extends BaseTest {
+
+    VideoPageSteps videoSteps = new VideoPageSteps();
+
+    public List<EventTalkResponseCard> getEventTalks() {
+        EventCardTalkFilterHelper eventHelper = new EventCardTalkFilterHelper();
+        EventFilterQueryParams eventQueryBuilder = new EventFilterQueryParams(eventHelper.getAllFilters());
+        HashMap<String, Object> queryParams = eventQueryBuilder
+                .addParams("Media", "Video")
+                .addParams("Category", "Testing")
+                .addParams("Location", "Belarus")
+                .addParams("Language", "ENGLISH")
+                .collect();
+        return eventHelper.getEventTalks(queryParams);
+    }
 
     @Test
     @DisplayName("Фильтрация докладов по категориям")
     public void shouldViewVideoFilteredByCategory() {
-        step("Пользователь переходит на вкладку Video", () -> {});
-        step("Пользователь нажимает на More Filters", () -> {});
+        videoSteps.userMoveToVideoTab();
+        videoSteps.userClickOnFilterShowMore();
         step("Пользователь выбирает: Category – Testing, Location – Belarus, Language – English, " +
-                "На развернутой вкладке фильтров", () -> {});
-        step("На странице отображаются карточки соответствующие правилам выбранных фильтров", () -> {});
+                "На развернутой вкладке фильтров", () -> {
+            videoSteps.userSelectCategoryFromFilter("Testing");
+            videoSteps.userSelectLocationFromFilter("Belarus");
+            videoSteps.userSelectLanguageFromFilter("ENGLISH");
+        });
+
+        List<EventTalkResponseCard> eventTalkCards = getEventTalks();
+        step("На странице отображаются карточки соответствующие правилам выбранных фильтров", () -> {
+            videoSteps.eventTalkCardsCountEqualResponseCards(eventTalkCards);
+            videoSteps.eventTalkCardsNamedEqualResponseCards(eventTalkCards);
+            videoSteps.eventTalkCardsLanguageEqualResponseCards(eventTalkCards);
+            videoSteps.eventTalkCardsHaveVideoMedia();
+        });
     }
 
     @Test
     @DisplayName("Поиск докладов по ключевому слову")
     public void shouldSearchVideoByKeyword() {
-        step("Пользователь переходит на вкладку Video", () -> {});
-        step("Пользователь вводит ключевое слово QA в поле поиска", () -> {});
-        step("На странице отображаются доклады, содержащие в названии ключевое слово поиска", () -> {});
+        videoSteps.userMoveToVideoTab();
+        videoSteps.userTypeKeywordToFilterSearch("QA");
+        videoSteps.eventTalkCardsContainSubstring("QA");
     }
 }
