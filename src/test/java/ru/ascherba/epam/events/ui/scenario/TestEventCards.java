170a697afd74eee01495d9878be14f0e6f08adde
Aleksandr Scherba <aleksandr.scherba@iqoption.com>
2021-02-18 23:17:50 +0300

    Add last tests

--- src/test/java/ru/ascherba/epam/events/ui/scenario/TestEventCards.java
+++ src/test/java/ru/ascherba/epam/events/ui/scenario/TestEventCards.java
@@ -1,42 +1,56 @@
 package ru.ascherba.epam.events.ui.scenario;
 
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.ascherba.epam.events.entities.events.Event;
+import ru.ascherba.epam.events.helpers.EventCardHelper;
+import ru.ascherba.epam.events.steps.EventPageSteps;
 
+import java.util.List;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 /**
  * Created by aleksandr.scherba on 23.01.2021
  */
+@Epic("EPAM Events")
 @Feature("Карточки мероприятий")
-public class TestEventCards {
+public class TestEventCards extends BaseTest {
+
+    EventPageSteps eventSteps = new EventPageSteps();
+    EventCardHelper eventCardHelper = new EventCardHelper();
 
     @Test
     @DisplayName("Просмотр карточек мероприятий")
     public void shouldHaveInformationAboutEvent() {
-        step("Пользователь переходит на вкладку events", () -> {});
-        step("Пользователь нажимает на Upcoming Events", () -> {});
-        step("На странице отображаются карточки предстоящих мероприятий", () -> {});
-        step("В карточке указана информация о мероприятии", () -> {
-            // Важно проверить порядок отображаемых блоков с информацией в карточке мероприятия
-            step("место проведения, язык", () -> {});
-            step("название мероприятия", () -> {});
-            step("дата мероприятия", () -> {});
-            step("информация о регистрации", () -> {});
-            step("список спикеров", () -> {});
-        });
+        eventSteps.userMoveToEventsTab();
+        eventSteps.userClickOnUpcomingEvents();
+        eventSteps.eventCardsPresentedOnPage();
+        List<Event> responseEvents = eventCardHelper
+                .getUpcomingResponse()
+                .events;
+        eventSteps.eventCardsEqualToResponseCards(responseEvents);
     }
 
     @Test
     @DisplayName("Просмотр детальной информации о мероприятии")
     public void shouldHaveDetailedInformationAboutEvent() {
-        step("Пользователь переходит на вкладку events", () -> {});
-        step("Пользователь нажимает на Upcoming Events", () -> {});
-        step("На странице отображаются карточки предстоящих мероприятий", () -> {});
-        step("Пользователь нажимает на любую карточку", () -> {});
-        step("Происходит переход на страницу с подробной информацией о мероприятии", () -> {});
+        eventSteps.userMoveToEventsTab();
+        eventSteps.userClickOnUpcomingEvents();
+        eventSteps.eventCardsPresentedOnPage();
+        String eventTitle = eventSteps.userClickOnRandomCard();
+        Event responseEvent = eventCardHelper.getUpcomingEvent(eventTitle);
+        eventSteps.openEventCardDetailPage(responseEvent);
         step("На странице с информацией о мероприятии отображается блок с кнопкой для регистрации, " +
-                "дата и время, программа мероприятия", () -> {});
+                "дата и время, программа мероприятия", () -> {
+            $(".evnt-panel-wrapper")
+                    .find(byText(responseEvent.title))
+                    .shouldBe(Condition.visible);
+        });
     }
 }
